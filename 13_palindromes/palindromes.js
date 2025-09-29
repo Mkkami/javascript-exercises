66affb24d21b2490984cdc9e0dfa92cccde46163
Michał Marciniak <michal.mar725@gmail.com>
2025-09-29 19:34:03 +0200

    finish 12-16

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let remove = ['.', ',', ' ', '!'];
+    string = string.replace([...remove], '');
+    for (s of remove) {
+        string = string.replaceAll(s, '');
+    }
+    string = string.toLowerCase();
+    if (string === reverse(string)) {
+        return true;
+    }
+    console.log(string + " " + reverse(string));
+    return false;
+    
 };
+
+function reverse(string) {
+    return string.split("").reverse().join("");
+}
+
+function reverseLoop(string) {
+    let str = "";
+    for (i = 0; i < string.length; i++) {
+        str = string.charAt(i) + str;
+    }
+    return str;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let year = new Date();
+    year = year.getFullYear();
 
+    let ans = arr.reduce((max, current) => {
+        let death;
+        if ("yearOfDeath" in current) {
+            death = current.yearOfDeath;
+        } else {
+            death = year;
+        }
+        let age = death - current.yearOfBirth;
+        if (max.age < age) {
+            max.age = age;
+            max.name = current.name;
+        }
+        return max;
+    }, {name:"", age: 0});
+
+    return ans;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
